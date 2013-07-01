73a599790690a8c2e1b880edd262c4620155ba7a
Andrew Stacey <stacey@math.ntnu.no>
2013-07-01 10:38:32 +0100

    More experiments with adding numbers to multiline equations.  The CSS and Javascript code are now split off into separate files.

---
+++ numberedeqns.js
@@ -0,0 +1,16 @@
+function moveEqNums() {
+    var dmaths = document.getElementsByClassName('displaymaths numbered align');
+    for (var i = 0; i < dmaths.length; i++) {
+	var dmath = dmaths[i];
+	if (dmath.tagName == 'table')
+	{
+	    var rows = dmath.firstChild.childNodes[1].children[0].firstChild.firstChild.children;
+	    var labels = dmath.firstChild.childNodes[2].firstChild.children;
+            for (var j = 0; j < rows.length; j++) {
+		labels[j].style.height = rows[j].scrollHeight + 'px';
+            }
+	}
+    }
+}
+
+window.onload = moveEqNums;
